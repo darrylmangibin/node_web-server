13dd4796431a3b1a3a26b49cfca606d0d8d1d943
Darryl <darrylmangibin@gmail.com>
2019-01-27 16:56:16 +0800

    configuring port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -50,6 +52,6 @@ app.get( '/bad', (req, res) => {
     })
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
 });
