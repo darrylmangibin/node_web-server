bc4fefbcb983a469572dd0b4d45f900fd6f41a05
Darryl <darrylmangibin@gmail.com>
2019-01-27 17:09:49 +0800

    Add project page

--- server.js
+++ server.js
@@ -46,6 +46,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    })
+})
+
 app.get( '/bad', (req, res) => {
     res.send({
         errorMessage: 'bad request'
